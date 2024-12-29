5a09bd8be0f170b1157d3b3d4e899361e01b23c9
Petar Nenov <petarnenovpetrov@gmail.com>
2024-12-30 00:40:43 +0200

    update: styles

--- apps/client/src/pages/MonitoringDetails.jsx
+++ apps/client/src/pages/MonitoringDetails.jsx
@@ -15,15 +15,15 @@ const MonitoringDetails = ({ useMonitoringDetailsViewModel = useMonitoringDetail
     return (
         <div className={styles.container}>
             <button className={styles.backButton} onClick={handleBack}>Back</button>
-            <h2 className={styles.title}>Monitoring Details</h2>
-            <p className={styles.detail}><strong>ID:</strong> {monitoringData.id}</p>
-            <p className={styles.detail}><strong>Source:</strong> {monitoringData.source}</p>
-            <p className={styles.detail}><strong>Message:</strong> {monitoringData.message}</p>
-            <p className={styles.detail}><strong>Stack Trace:</strong></p>
+            <h2 className={styles.title}>Monitoring Details: <span className={styles.errorMessage}>{monitoringData.message}</span></h2>
+            <p className={styles.detail}><strong className={styles.detailLabel}>ID:</strong> {monitoringData.id}</p>
+            <p className={styles.detail}><strong className={styles.detailLabel}>Source:</strong> {monitoringData.source}</p>
+            <p className={styles.detail}><strong className={styles.detailLabel}>Message:</strong> {monitoringData.message}</p>
+            <p className={styles.detail}><strong className={styles.detailLabel}>Stack Trace:</strong></p>
             <pre className={styles.stackTrace}>{monitoringData.stackTrace}</pre>
-            <p className={styles.detail}><strong>Steps to Reproduce:</strong></p>
+            <p className={styles.detail}><strong className={styles.detailLabel}>Steps to Reproduce:</strong></p>
             <pre className={styles.stepsToReproduce}>{JSON.stringify(JSON.parse(monitoringData.stepsToReproduce).sort(stepsToReproducePredicate), null, 2)}</pre>
-            <p className={styles.detail}><strong>Created At:</strong> {new Date(monitoringData.createdAt).toLocaleString()}</p>
+            <p className={styles.detail}><strong className={styles.detailLabel}>Created At:</strong> {new Date(monitoringData.createdAt).toLocaleString()}</p>
         </div>
     );
 };
