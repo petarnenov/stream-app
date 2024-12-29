13a7aecf62ff9983dbf9f665af80b3f6a9cbe6d8
Petar Nenov <petarnenovpetrov@gmail.com>
2024-12-29 14:56:26 +0200

    clean: webscoket

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,12 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { WsAdapter } from '@nestjs/platform-ws';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  app.useWebSocketAdapter(new WsAdapter(app));
   app.setGlobalPrefix('api');
   app.useGlobalPipes(
     new ValidationPipe({
